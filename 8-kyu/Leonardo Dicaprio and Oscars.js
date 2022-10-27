4eae72d5c0d53625c1c5f77e411b19b2fb05c626
Suric1 <igorbronko@gmail.com>
2022-10-27 21:44:00 +0300

    CodeWars Solution

---
+++ 8-kyu/Leonardo Dicaprio and Oscars.js
@@ -0,0 +1,11 @@
+// Leonardo Dicaprio and Oscars
+
+
+function leo(oscar){
+ return oscar === 86 ? "Not even for Wolf of wallstreet?!"
+	 : oscar === 88 ? "Leo finally won the oscar! Leo is happy"
+		 : oscar > 88 ? "Leo got one already!"
+			 : "When will you give Leo an Oscar?"
+}
+
+
