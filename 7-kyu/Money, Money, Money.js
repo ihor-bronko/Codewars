6c7b20b7395b7ef36a3a28e8d39fa48c17dc86e0
Suric1 <igorbronko@gmail.com>
2022-11-18 23:47:29 +0200

    CodeWars Solution

---
+++ 7-kyu/Money, Money, Money.js
@@ -0,0 +1,10 @@
+// Money, Money, Money
+
+function calculateYears(principal, interest, tax, desired) {
+	let year = 0
+	while (principal < desired) {
+		year++
+		principal = principal + (principal * interest - principal * interest * tax)
+	}
+	return year
+}
