e5372a4105dca1c701a218fe86b935e2437e944f
Suric1 <igorbronko@gmail.com>
2022-11-26 23:50:25 +0200

    CodeWars Solution

---
+++ 7-kyu/Make a function that does arithmetic!.js
@@ -0,0 +1,14 @@
+// Make a function that does arithmetic!
+
+function arithmetic(a, b, operator) {
+	switch (operator) {
+		case 'add':
+			return a + b
+		case 'subtract':
+			return a - b
+		case 'multiply':
+			return a * b
+		case 'divide':
+			return a / b
+	}
+}
